97853fb44067a5cbb7510c2afa427aa9b33f7ba4
Eoghan Reilly <reillyeo@Eoghans-MacBook-Air.local>
2023-11-22 21:37:26 +0100

    Add option to generate image from prompt

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div>
       <h1>Computer Vision</h1>
-      <p>Enter a URL to an image to analyze it.</p>
+      <p>Enter a URL to an image to analyze it, or a text prompt for image generation.</p>
       <input type="text" />
       <button>Analyze</button>
+      <button>Generate</button>
     </div>
   );
 }
